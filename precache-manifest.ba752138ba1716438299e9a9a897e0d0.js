5055130bc21bfa0582c37f84f52fb1074a70e073
Jo Bo-hwa <jobong212@gmail.com>
2019-11-01 16:23:54 +0900

    Updates

---
+++ precache-manifest.ba752138ba1716438299e9a9a897e0d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a66b054756aac782253d8c5c9354f3f",
+    "url": "/movie_app_bh/index.html"
+  },
+  {
+    "revision": "9d5d3ef39f6cf40374de",
+    "url": "/movie_app_bh/static/css/main.1bcf055a.chunk.css"
+  },
+  {
+    "revision": "755e962a5d4addd5fee0",
+    "url": "/movie_app_bh/static/js/2.88c3de90.chunk.js"
+  },
+  {
+    "revision": "9d5d3ef39f6cf40374de",
+    "url": "/movie_app_bh/static/js/main.d2bcb4e0.chunk.js"
+  },
+  {
+    "revision": "491749238b0c5f614f98",
+    "url": "/movie_app_bh/static/js/runtime-main.9112d5f2.js"
+  }
+]);
